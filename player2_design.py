30b0a88e17ea39e512e2b5a3c0ebaf92d741365c
Om2013 <omradhakrishnan123456@outlook.com>
2025-09-01 21:25:27 -0400

    final commit

--- player2_design.py
+++ player2_design.py
@@ -1,19 +1,20 @@
 import pygame 
+
 class Player2Paddle(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load("player_ping_pong_bat.png")
-        new_player2_image_size=100,100
-        self.image=pygame.transform.scale(self.image,new_player2_image_size)
+        new_player2_image_size = (100, 100)
+        self.image = pygame.transform.scale(self.image, new_player2_image_size)
         self.rect = self.image.get_rect(center=(x, y))
         self.score = 0
         self.speed = 5
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
+        if keys[pygame.K_UP]:   # Arrow up
             self.rect.y -= self.speed
-        if keys[pygame.K_s]:
+        if keys[pygame.K_DOWN]: # Arrow down
             self.rect.y += self.speed
 
         # Keep paddle on screen
